5156e8873271fc9afab48c5ba2abf6080451460d
木下翔太 <kinoshitashouta@kinoshitashouta-no-MacBook-Pro.local>
2020-02-05 18:29:24 +0900

    Improve

---
+++ main.js
@@ -0,0 +1,24 @@
+// -----------------------------------------------------------------------------
+// モジュールのインポート
+const server = require("express");
+const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+
+// -----------------------------------------------------------------------------
+// パラメータ設定
+const line_config = {
+    channelAccessToken: "9DRIPIKSnTrrZdC2Y2hhOaUuc4EWm5vOQlIj6dlp572v1OPbwyFX55ToiH0Av86aMMMBIdM30GbfjQAkZyFd2Ja0RLx6dv4/3ioaT2FESszmPiJdgMoMukDJJb4S9/0BR+TUu5A0cIDbGgj5fPezmQdB04t89/1O/w1cDnyilFU=",
+    channelSecret: "d9d4d0d7d5a21bccfe35edd3d217e9d4"
+
+};
+
+// -----------------------------------------------------------------------------
+// Webサーバー設定
+server.listen(process.env.PORT || 3000);
+
+
+// -----------------------------------------------------------------------------
+// ルーター設定
+server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+    res.sendStatus(200);
+    console.log(req.body);
+});
