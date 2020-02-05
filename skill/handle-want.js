7a2b83abb4edbc2c1462f7dc4a71f41e04e14e9f
木下翔太 <kinoshitashouta@kinoshitashouta-no-MacBook-Pro.local>
2020-02-05 22:01:56 +0900

    Improve

--- skill/handle-want.js
+++ skill/handle-want.js
@@ -1,24 +1,26 @@
 module.exports = class HandleWant{
   constructor() {
     this.required_parameter = {
-      type: {
-        type: "template",
-        altText: "なにしたいの？",
-        template: {
-          type: "buttons",
-          text: "なにしたいの？",
-          actions: [
-            {type:"postback", label:"see", data:"see"},
-            {type:"postback", label:"buy", data:"buy"},
-            {type:"postback", label:"get", data:"get"},
-            {type:"postback", label:"go", data:"go"}
-          ]
+      verb_type: {
+        message_to_confirm: {
+          type: "template",
+          altText: "なにしたいの？",
+          template: {
+            type: "buttons",
+            text: "なにしたいの？",
+            actions: [
+              {type:"postback", label:"see", data:"see"},
+              {type:"postback", label:"buy", data:"buy"},
+              {type:"postback", label:"get", data:"get"},
+              {type:"postback", label:"go", data:"go"}
+            ]
+          }
         }
       },
       word: {
         massage_to_comfirm: {
-          "type": "text",
-          "text": "What are you want?"
+          type: "text",
+          text: "What are you want?"
         }
       },
      // image: {
